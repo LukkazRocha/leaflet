5838496225e620d5370356847cbe8a57717d580b
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-10-05 20:16:32 -0300

    create geojsonEmpresas

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
-var Marker = L.marker([-23.417510791934347, -51.941398188500806], {title: 'Datlo'})
-
-Marker.bindPopup("Datlo")
-
-Marker.addTo(map);
-
 var circle = L.circle([-23.403808603949706, -51.93971503965945], {
     color: '#000000',
     fillColor: '#294F6D',
@@ -18,33 +12,60 @@ var circle = L.circle([-23.403808603949706, -51.93971503965945], {
     radius: 750
 }).addTo(map);
 
-/* L.geoJSON(geojsonFeature, {
-    color: '#000000',
-    fillColor: 'rgb(255, 246, 24)',
-    fillOpacity: 0.5,
+/* var layer = L.geoJSON(geojsonCidades, {
+  fillColor: '#00f',
+  fillOpacity: '0.5',
+  weight: 2,
+  color: '#000',
+  onEachFeature: function (feature, layer) {
+    var name = feature.properties.name
+    layer.bindPopup(name)
+    layer.on('mouseover', function () {
+      this.setStyle({
+        'fillColor': '#fff618'
+      })
+      layer.openPopup()
+    })
+    layer.on('mouseout', function () {
+      this.setStyle({
+        'fillColor': '#00f'
+      })
+      layer.closePopup()
+    })
+    layer.on('click', function () {
+      this.setStyle({
+        'fillColor': '#fff618'
+      })     
+    })
+  }
+}).addTo(map)
 
-}).addTo(map); */
+layer  = L.geoJSON(geojsonEmpresas).addTo(map) */
 
-var layer = new L.GeoJSON(geojsonFeature, {
+var layer = L.geoJSON(geojsonCidades, {           
     fillOpacity: 0.5,
-    fillColor: '#00f',
-    weight: '2',
-    color: '#000',
+    weight: 2,
     onEachFeature: function (feature, layer) {
-      layer.on('mouseover', function () {
-        this.setStyle({            
-            'fillColor': '#FFF618',
-        });
-      });
-      layer.on('mouseout', function () {
-        this.setStyle({
-            'fillColor': '#fff'
-        });
-      });
-      layer.on('click', function () {
-        // Let's say you've got a property called url in your geojsonfeature:
-        console.log(feature)
-        layer.bindPopup("Popup de teste")
-      });      
+        var name = feature.properties.name
+        layer.bindPopup(name)        
+        layer.on ('mouseover', function () {
+            layer.openPopup()
+            this.setStyle({
+                'fillColor': '#fff618'
+            })
+        })
+        layer.on ('mouseout', function () {
+            layer.closePopup()
+            this.setStyle({
+                'fillColor': null
+            })
+        })
+        layer.on ('click', function onClick () {
+            this.setStyle({
+                'fillColor': '#fff618'
+            })
+        })
     }
-  }).addTo(map);
+}).addTo(map) 
+
+var layerJob = L.geoJSON(geojsonEmpresas).addTo(map)
