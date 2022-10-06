8dcca550295b56394d98a98f442af675f96a3443
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-10-06 07:51:21 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -12,36 +12,6 @@ var circle = L.circle([-23.403808603949706, -51.93971503965945], {
     radius: 750
 }).addTo(map);
 
-/* var layer = L.geoJSON(geojsonCidades, {
-  fillColor: '#00f',
-  fillOpacity: '0.5',
-  weight: 2,
-  color: '#000',
-  onEachFeature: function (feature, layer) {
-    var name = feature.properties.name
-    layer.bindPopup(name)
-    layer.on('mouseover', function () {
-      this.setStyle({
-        'fillColor': '#fff618'
-      })
-      layer.openPopup()
-    })
-    layer.on('mouseout', function () {
-      this.setStyle({
-        'fillColor': '#00f'
-      })
-      layer.closePopup()
-    })
-    layer.on('click', function () {
-      this.setStyle({
-        'fillColor': '#fff618'
-      })     
-    })
-  }
-}).addTo(map)
-
-layer  = L.geoJSON(geojsonEmpresas).addTo(map) */
-
 var layer = L.geoJSON(geojsonCidades, {           
     fillOpacity: 0.5,
     weight: 2,
